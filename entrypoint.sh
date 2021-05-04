980a6d7eb089e8d1a96c48ef60fd17feb152ad9d
wangyi2005 <wangyi2005@users.noreply.github.com>
2021-05-04 22:34:35 +0800

    Update and rename context.xml to entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+wget -O ss.tar.xz https://github.com/shadowsocks/shadowsocks-rust/releases/download/v$SSVER/shadowsocks-v$SSVER.x86_64-unknown-linux-gnu.tar.xz
+wget -O vp.tar.xz https://github.com/shadowsocks/v2ray-plugin/releases/download/v$v2pVER/v2ray-plugin-linux-amd64-v$v2pVER.tar.gz
+tar -xvf ss.tar.xz 
+tar -xvf vp.tar.xz 
+mv v2ray-plugin_linux_amd64 v2p
+chmod +x ssserver v2p
+./ssserver -s 0.0.0.0:$PORT -m $method -k $password --plugin "./v2p" --plugin-opts "server"
