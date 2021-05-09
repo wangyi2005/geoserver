7e40d4c297c0953461c0bd69177a52d48ddae917
wangyi2005 <wangyi2005@users.noreply.github.com>
2021-05-09 19:57:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /ss
-wget -q -O soc http://github.com/wangyi2005/ss-docker/blob/master/soc-v$SOCVER
-wget -q -O v2p http://github.com/wangyi2005/ss-docker/blob/master/v2p-v$V2PVER
+wget -q -O soc http://github.com/wangyi2005/ss-docker/raw/master/soc-v$SOCVER
+wget -q -O v2p http://github.com/wangyi2005/ss-docker/raw/master/v2p-v$V2PVER
 chmod +x soc v2p
 ./soc -s 0.0.0.0:8080 -m $method -k $password --plugin "./v2p" --plugin-opts "server"
