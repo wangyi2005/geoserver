30d70846c3c02308c16a9e83edde1ebd4988dc22
wangyi2005 <wangyi2005@users.noreply.github.com>
2021-05-09 13:50:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /ss
-wget -O ss.tar.xz https://github.com/shadowsocks/shadowsocks-rust/releases/download/v$SSVER/shadowsocks-v$SSVER.x86_64-unknown-linux-gnu.tar.xz
-wget -O vp.tar.xz https://github.com/shadowsocks/v2ray-plugin/releases/download/v$v2pVER/v2ray-plugin-linux-amd64-v$v2pVER.tar.gz
+curl -o ss.tar.xz https://github.com/shadowsocks/shadowsocks-rust/releases/download/v$SSVER/shadowsocks-v$SSVER.x86_64-unknown-linux-gnu.tar.xz
+curl -o vp.tar.xz https://github.com/shadowsocks/v2ray-plugin/releases/download/v$v2pVER/v2ray-plugin-linux-amd64-v$v2pVER.tar.gz
 tar -xvf ss.tar.xz 
 tar -xvf vp.tar.xz 
 mv v2ray-plugin_linux_amd64 v2p
