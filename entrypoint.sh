5891b8ab7ed64ec4796a71bbdeb7cd4fb1ab448e
wangyi2005 <wangyi2005@users.noreply.github.com>
2021-10-03 11:09:28 +0800

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-cd /ss
-wget -q -O soc https://gd.wangyi.ml/edge/soc-v$SOCVER
-wget -q -O v2p https://gd.wangyi.ml/edge/v2p-v$V2PVER
-chmod +x soc v2p
-./soc -s 0.0.0.0:8080 -m $method -k $password --plugin "./v2p" --plugin-opts "server"
